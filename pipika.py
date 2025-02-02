947ba17d9788e6df1a9c9b15953433982c166337
SerheyBublik <zf.41@tut.by>
2025-02-02 20:18:40 +0300

    кака-то ошибка

---
+++ pipika.py
@@ -0,0 +1,54 @@
+import telebot
+import webbrowser
+from telebot import types
+bot = telebot.TeleBot('7698187197:AAFFNNogj49D1OxUMThQHozGeYOTnKiJKgA')
+
+
+@bot.message_handler(content_types=['photo'])
+def get_photo(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton('пупи тут', url='https://www.youtube.com/shorts/3cOnrftz5uY')
+    markup.row(btn1)
+    btn2 = types.InlineKeyboardButton('удалить фото', callback_data='delete')
+    btn3 = types.InlineKeyboardButton('изменить текст', callback_data='edit')
+    markup.row(btn2,btn3)
+    bot.reply_to(message, 'Какое красивое фото!', reply_markup=markup)
+
+
+@bot.callback_query_handlers(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == "delete":
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
+    elif callback.data == "edit":
+       bot.edit_message_text('Edit text', callback.message.chat.id, callback.message.message_id)
+
+
+@bot.message_handler(commands=['site', 'website'])
+def site(message):
+    webbrowser.open('https://www.youtube.com/shorts/3cOnrftz5uY')
+
+
+@bot.message_handler(commands=['inf'])
+def main(message):
+    bot.send_message(message.chat.id, message)
+
+
+@bot.message_handler(commands=['start', 'main', 'hello'])
+def main(message):
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}, от пипикалки, написанной на Python!')
+
+
+@bot.message_handler(commands=['help'])
+def main(message):
+    bot.send_message(message.chat.id, '<b>Тут</b> <em><u>никто</u></em> не помахает только попипикают', parse_mode='html')
+
+
+@bot.message_handler()
+def info(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id,f'Привет, {message.from_user.first_name}, риветриветривет')
+    elif message.text.lower() == 'id':
+        bot.reply_to(message, f'ID {message.from_user.id}')
+
+
+bot.polling(none_stop=True)
